92a148eb3d65de0f84418db41b1a8eac95e3912d
jorge <joenco@debian.(none)>
2013-06-01 19:46:54 -0430

    Se completo el archivo para calcular fifo, añadiendo todas las distribuciones: Uniforme, Exponencial y Normal

--- calculo.py
+++ calculo.py
@@ -27,43 +27,58 @@
 
 import gtk, pango
 import random
+import numpy as np
+import scipy.stats as st
 
 class CalculoFifo(gtk.HBox):
-    def calcular(self, n, te, tr, funcion1, funcion2, funcion3):
+    def calcular(self, n, te, tp, funcion1, funcion2):
       self.cola_procesos = []
       self.total_llegada = 0
       self.promedio_llegada=0
-      self.total_servicio=0
-      self.promedio_servicio=0
+      self.total_proceso=0
+      self.promedio_llegada=0
+      self.promedio_proceso=0
       random.seed(5000)
-      self.n = int(n)
-      self.te = int(te)
-      self.tr = int(tr)
+      self.n = float(n) #núermo de procesos.
+      self.te = float(te) #tiempo de llegada de los procesos
+      self.tp = float(tp) #tiempo de duración de los procesos.
       self.func_llegada = funcion1
-      self.func_servicio = funcion2
-
-      #random.randint(0, self.te)
-      if self.func_llegada == 'Constante':
-        for i in xrange(self.n):
-          self.cola_procesos.append([])#agregamos un objeto de tipo lista a la cola
-          self.cola_procesos[i].append(i)
-          self.cola_procesos[i].append(self.te)
-          self.cola_procesos[i].append(self.tr)
-          print ''
+      self.func_proceso = funcion2
 
       for i in xrange(self.n):
-        self.total_llegada += self.cola_procesos[i][1]
-        self.total_servicio += self.cola_procesos[i][2]
+        self.cola_procesos.append([])#agregamos un objeto de tipo lista a la cola
+        self.cola_procesos[i].append(i)
+        if self.func_proceso == 'Constante':
+          self.cola_procesos[i].append(self.tp)
+        elif self.func_proceso == 'Uniforme':
+          self.cola_procesos[i].append(np.random.randint(0, self.tp))
+        elif self.func_proceso == 'Exponencial':
+          self.cola_procesos[i].append(np.random.exponential(self.tp))
+        elif self.func_proceso == 'Normal':
+          self.cola_procesos[i].append(st.norm.cdf(self.tp))
+
+        if self.func_llegada == 'Constante':
+          self.cola_procesos[i].append(self.te)
+        elif self.func_llegada == 'Uniforme':
+          self.cola_procesos[i].append(np.random.randint(0, self.tp))
+        elif self.func_llegada == 'Exponencial':
+          self.cola_procesos[i].append(np.random.exponential(self.te))
+        elif self.func_llegada == 'Normal':
+          self.cola_procesos[i].append(st.norm.cdf(self.te))
+
+      for i in xrange(self.n):
+        self.total_proceso += self.cola_procesos[i][1]
+        self.total_llegada += self.cola_procesos[i][2]
 
       for i in xrange(self.n):
         print self.cola_procesos[i][0],'\t\t',self.cola_procesos[i][1],'\t\t',self.cola_procesos[i][2]
 
       self.promedio_llegada = float(self.total_llegada/self.n)
-      self.promedio_servicio = int(self.total_servicio/self.n)
+      self.promedio_proceso = float(self.total_proceso/self.n)
       print 'Tiempo total de llegada: ',self.total_llegada
-      print 'Tiempo total de servicio: ',self.total_servicio
+      print 'Tiempo total de proceso: ',self.total_proceso
       print 'Tiempo promedio de llegada: ',(self.promedio_llegada)
-      print 'Tiempo promedio de servicio: ',self.promedio_servicio
+      print 'Tiempo promedio de proceso: ',(self.promedio_proceso)
 
         #return self.total_llegada, self.promedio_llegada
-      return self.total_llegada, self.promedio_llegada
+      return self.total_llegada, self.promedio_llegada, self.total_proceso, self.promedio_proceso
