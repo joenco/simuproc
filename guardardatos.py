08f69016e27aac3719072082c79f2e3de8e348ca
jorge <joenco@debian.(none)>
2013-06-09 07:26:03 -0430

    guarda los datos del usuario en la carpeta data

---
+++ guardardatos.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Guardar():
+    def Guardar(self, datos, a):
+      self.nombre = datos
+      self.a = int(a)
+
+      if self.a==0:
+        self.datos = open('data/FCFS.dat', 'w')
+      elif self.a==1:
+        self.datos = open('data/SJF.dat', 'w')
+      elif self.a==2:
+        self.datos = open('data/PPSJF.dat', 'w')
+      elif self.a==3:
+        self.datos = open('data/RoundRobin.dat', 'w')
+
+      n = len(self.nombre)
+      for i in xrange(n):
+        for j in xrange(3):
+          data=str(self.nombre[i][j])
+          self.datos.write(' '+data)
+        self.datos.write('\n')
+
+      self.datos.close()
