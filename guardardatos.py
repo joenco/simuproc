ec6bc494f797c0f986d2cf85c083d3fad5fdfb7f
jorge <joenco@debian.(none)>
2013-07-13 11:16:59 -0430

    almacenando todos los datos de la corrida

--- guardardatos.py
+++ guardardatos.py
@@ -2,25 +2,32 @@
 # -*- coding: utf-8 -*-
 
 class Guardar():
-    def Guardar(self, datos, a):
-      self.nombre = datos
+    def Guardar(self, datos, datos1, a):
+      self.datos = datos #datos de tiempo de espera.
+      self.datos1 = datos1 # datos de tiempo del cpu y tiempo de llegada.
       self.a = int(a)
 
       if self.a==0:
-        self.datos = open('data/FCFS.dat', 'w')
+        self.data = open('data/FCFS.dat', 'w')
       elif self.a==1:
-        self.datos = open('data/SJF.dat', 'w')
+        self.data = open('data/SJF.dat', 'w')
       elif self.a==2:
-        self.datos = open('data/PPSJF.dat', 'w')
+        self.data = open('data/PPSJF.dat', 'w')
       elif self.a==3:
-        self.datos = open('data/RoundRobin.dat', 'w')
+        self.data = open('data/RoundRobin.dat', 'w')
 
-      n = len(self.nombre)
+      n = len(self.datos)
+
+      dat = 'N  |  T.E  |  T.CPU  |  T.L  |'
+      self.data.write(dat)
+      self.data.write('\n')
       for i in xrange(n):
-        x1 = str(self.nombre[i][0])
-        y1 = str(self.nombre[i][1])
-        data = x1+'  |  '+y1+'  |'
-        self.datos.write(data)
-        self.datos.write('\n')
+        x1 = str(self.datos[i][0]) #Número de procesos
+        y1 = str(self.datos[i][1]) # tiempo de espera
+        a1 = str(self.datos1[i][1]) # tiempo de cpu
+        z1 = str(self.datos1[i][2]) # tiempo de llegada
+        dat = x1+'  |  '+y1+'  |  '+a1+'  |  '+z1+'  |'
+        self.data.write(dat)
+        self.data.write('\n')
 
-      self.datos.close()
+      self.data.close()
