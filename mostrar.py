7bebf8e38923627653153790fbd77d13aa2f2de4
jorge <joenco@debian.(none)>
2013-07-12 22:40:12 -0430

    colocando una barra de progreso, mientras se hacen los calculos

--- mostrar.py
+++ mostrar.py
@@ -25,13 +25,16 @@
 #
 # CODE IS POETRY
 
-import gtk, pango
+import gtk, pango, gobject
 import gtk.gdk
 from threading import Thread, Semaphore
+from calculo import Algoritmos
 from simulacion import Simulacion, ventana
 from graficos import Graficar
 
+calculo = Algoritmos()
 Graficar = Graficar()
+
 class MostrarResultados(gtk.HBox):
     def __init__(self, CFG):
         gtk.HBox.__init__(self)
@@ -42,130 +45,135 @@ class MostrarResultados(gtk.HBox):
         size = pango.AttrSize(18000, 0, -1)
         attr.insert(size)
 
-        self.lbltitle1 = gtk.Label("Resultados de la corrida")
+        self.lbltitle1 = gtk.Label("Espere mientras hacemos los calculos")
         self.lbltitle1.set_alignment(0, 0.5)
         self.lbltitle1.set_attributes(attr)
         table.attach(self.lbltitle1, 0, 2, 0, 1)
 
-        self.txt1 = gtk.Label("Algoritmo")
+        n=int(CFG['nproceso'])/8
+        self.intervalo = 0.02
+        if n<100:
+            n=100
+            self.intervalo=0.03
+
+        self.pbar = gtk.ProgressBar()
+        self.pbar.show()
+        table.attach(self.pbar, 0, 6, 3, 4)
+        self.timer = gobject.timeout_add (n, self.progress_timeout, CFG, self)
+
+        self.txt1 = gtk.Label(' ')
         self.txt1.set_alignment(0, 0.5)
         table.attach(self.txt1, 0, 1, 1, 2)
 
-        self.txt2 = gtk.Label("Número de procesos")
+        self.txt2 = gtk.Label(' ')
         self.txt2.set_alignment(0, 0.5)
         table.attach(self.txt2, 1, 2, 1, 2)
 
-        self.txt3 = gtk.Label("Uso del CPU")
+        self.txt3 = gtk.Label(' ')
         self.txt3.set_alignment(0, 0.5)
         table.attach(self.txt3, 2, 3, 1, 2)
 
-        self.txt4 = gtk.Label("Tiempo promedio del CPU")
+        self.txt4 = gtk.Label(' ')
         self.txt4.set_alignment(0, 0.5)
         table.attach(self.txt4, 3, 4, 1, 2)
 
-        self.txt5 = gtk.Label("Tiempo promedio de espera")
+        self.txt5 = gtk.Label(' ')
         self.txt5.set_alignment(0, 0.5)
         table.attach(self.txt5, 4, 5, 1, 2)
-
-        self.txt6 = gtk.Label("Gráfica")
-        self.txt6.set_alignment(0, 0.5)
-        table.attach(self.txt6, 5, 6, 1, 2)
 
         self.listafuncion = gtk.ListStore(str)
 
         if CFG['fifo']==True:
-          self.txtcfifo = gtk.Label("FCFS")
-          self.txtcfifo.set_alignment(0, 0.5)
-          table.attach(self.txtcfifo, 0, 1, 2, 3)
+          self.txtfifo = gtk.Label(' ')
+          self.txtfifo.set_alignment(0, 0.5)
+          table.attach(self.txtfifo, 0, 1, 2, 3)
 
-          self.np = gtk.Label(CFG['nproceso'])
-          self.np.set_alignment(0, 0.5)
-          table.attach(self.np, 1, 2, 2, 3)
+          self.txtfifo1 = gtk.Label(' ')
+          self.txtfifo1.set_alignment(0, 0.5)
+          table.attach(self.txtfifo1, 1, 2, 2, 3)
 
-          self.tejecucion = gtk.Label(CFG['calculofifo'][0])
-          self.tejecucion.set_alignment(0, 0.5)
-          table.attach(self.tejecucion, 2, 3, 2, 3)
+          self.txtfifo2 = gtk.Label(' ')
+          self.txtfifo2.set_alignment(0, 0.5)
+          table.attach(self.txtfifo2, 2, 3, 2, 3)
 
-          self.tpromedio = gtk.Label(CFG['calculofifo'][1])
-          self.tpromedio.set_alignment(0, 0.5)
-          table.attach(self.tpromedio, 3, 4, 2, 3)
+          self.txtfifo3 = gtk.Label(' ')
+          self.txtfifo3.set_alignment(0, 0.5)
+          table.attach(self.txtfifo3, 3, 4, 2, 3)
 
-          self.tproceso = gtk.Label(CFG['calculofifo'][2])
-          self.tproceso.set_alignment(0, 0.5)
-          table.attach(self.tproceso, 4, 5, 2, 3)
+          self.txtfifo4 = gtk.Label(' ')
+          self.txtfifo4.set_alignment(0, 0.5)
+          table.attach(self.txtfifo4, 4, 5, 2, 3)
 
           self.listafuncion.append(["FCFS"])
-          self.datos = CFG['calculofifo'][3]
 
         # SJF
         if CFG['menortiempo']==True:
-          self.txtmt = gtk.Label("SJF")
+          self.txtmt = gtk.Label(' ')
           self.txtmt.set_alignment(0, 0.5)
           table.attach(self.txtmt, 0, 1, 3, 4)
 
-          self.np = gtk.Label(CFG['nproceso'])
-          self.np.set_alignment(0, 0.5)
-          table.attach(self.np, 1, 2, 3, 4)
+          self.txtmt1 = gtk.Label(' ')
+          self.txtmt1.set_alignment(0, 0.5)
+          table.attach(self.txtmt1, 1, 2, 3, 4)
 
-          self.tejecucion = gtk.Label(CFG['mtiempo'][0])
-          self.tejecucion.set_alignment(0, 0.5)
-          table.attach(self.tejecucion, 2, 3, 3, 4)
+          self.txtmt2 = gtk.Label(' ')
+          self.txtmt2.set_alignment(0, 0.5)
+          table.attach(self.txtmt2, 2, 3, 3, 4)
 
-          self.tpromedio = gtk.Label(CFG['mtiempo'][1])
-          self.tpromedio.set_alignment(0, 0.5)
-          table.attach(self.tpromedio, 3, 4, 3, 4)
+          self.txtmt3 = gtk.Label(' ')
+          self.txtmt3.set_alignment(0, 0.5)
+          table.attach(self.txtmt3, 3, 4, 3, 4)
 
-          self.tproceso = gtk.Label(CFG['mtiempo'][2])
-          self.tproceso.set_alignment(0, 0.5)
-          table.attach(self.tproceso, 4, 5, 3, 4)
+          self.txtmt4 = gtk.Label(' ')
+          self.txtmt4.set_alignment(0, 0.5)
+          table.attach(self.txtmt4, 4, 5, 3, 4)
 
           self.listafuncion.append(["SJF"])
-          self.datos1 = CFG['mtiempo'][3]
         # Round Robin
         if CFG['roundrobin']==True:
-          self.txtmt = gtk.Label("RR")
-          self.txtmt.set_alignment(0, 0.5)
-          table.attach(self.txtmt, 0, 1, 4, 5)
+          self.txtrr = gtk.Label(' ')
+          self.txtrr.set_alignment(0, 0.5)
+          table.attach(self.txtrr, 0, 1, 4, 5)
 
-          self.np = gtk.Label(CFG['nproceso'])
-          self.np.set_alignment(0, 0.5)
-          table.attach(self.np, 1, 2, 4, 5)
+          self.txtrr1 = gtk.Label(' ')
+          self.txtrr1.set_alignment(0, 0.5)
+          table.attach(self.txtrr1, 1, 2, 4, 5)
 
-          self.tejecucion = gtk.Label(CFG['calculorr'][0])
-          self.tejecucion.set_alignment(0, 0.5)
-          table.attach(self.tejecucion, 2, 3, 4, 5)
+          self.txtrr2 = gtk.Label(' ')
+          self.txtrr2.set_alignment(0, 0.5)
+          table.attach(self.txt2, 2, 3, 4, 5)
 
-          self.tpromedio = gtk.Label(CFG['calculorr'][1])
-          self.tpromedio.set_alignment(0, 0.5)
-          table.attach(self.tpromedio, 3, 4, 4, 5)
+          self.txtrr3 = gtk.Label(' ')
+          self.txtrr3.set_alignment(0, 0.5)
+          table.attach(self.txtrr3, 3, 4, 4, 5)
 
-          self.tproceso = gtk.Label(CFG['calculorr'][2])
-          self.tproceso.set_alignment(0, 0.5)
-          table.attach(self.tproceso, 4, 5, 4, 5)
+          self.txtrr4 = gtk.Label(' ')
+          self.txtrr4.set_alignment(0, 0.5)
+          table.attach(self.txtrr4, 4, 5, 4, 5)
 
           self.listafuncion.append(["RR"])
 
         # PSJF
         if CFG['soprtunidad']==True:
-          self.txtmt = gtk.Label("PSJF")
-          self.txtmt.set_alignment(0, 0.5)
-          table.attach(self.txtmt, 0, 1, 5, 6)
+          self.txtso = gtk.Label(' ')
+          self.txtso.set_alignment(0, 0.5)
+          table.attach(self.txtso, 0, 1, 5, 6)
 
-          self.np = gtk.Label(CFG['nproceso'])
-          self.np.set_alignment(0, 0.5)
-          table.attach(self.np, 1, 2, 5, 6)
+          self.txtso1 = gtk.Label(' ')
+          self.txtso1.set_alignment(0, 0.5)
+          table.attach(self.txtso1, 1, 2, 5, 6)
 
-          self.tejecucion = gtk.Label(CFG['psjf'][0])
-          self.tejecucion.set_alignment(0, 0.5)
-          table.attach(self.tejecucion, 2, 3, 5, 6)
+          self.txtso2 = gtk.Label(' ')
+          self.txtso2.set_alignment(0, 0.5)
+          table.attach(self.txtso2, 2, 3, 5, 6)
 
-          self.tpromedio = gtk.Label(CFG['psjf'][1])
-          self.tpromedio.set_alignment(0, 0.5)
-          table.attach(self.tpromedio, 3, 4,5, 6)
+          self.txtso3 = gtk.Label(' ')
+          self.txtso3.set_alignment(0, 0.5)
+          table.attach(self.txtso3, 3, 4,5, 6)
 
-          self.tproceso = gtk.Label(CFG['psjf'][2])
-          self.tproceso.set_alignment(0, 0.5)
-          table.attach(self.tproceso, 4, 5, 5, 6)
+          self.txtso4 = gtk.Label(' ')
+          self.txtso4.set_alignment(0, 0.5)
+          table.attach(self.txtso4, 4, 5, 5, 6)
 
           self.listafuncion.append(["PSJF"])
 
@@ -175,10 +183,12 @@ class MostrarResultados(gtk.HBox):
         table.attach(self.listaalgoritmo, 0, 1, 6, 7)
 
         self.ver = gtk.Button("Ver gráfica")
+        self.ver.hide()
         self.ver.connect('clicked', self.Ver)
         table.attach(self.ver, 2, 3, 6, 7)
 
         self.simulacion = gtk.Button("Ver Simulación")
+        self.simulacion.hide()
         self.simulacion.connect('clicked', self.Simulacion)
         table.attach(self.simulacion, 4, 5, 6, 7)
 
@@ -203,3 +213,64 @@ class MostrarResultados(gtk.HBox):
           hilo = Simulacion(win.label, win.label1, win.label3, win.label5, self.n, x, semaforo)
           hilo.start()
       win.show()
+
+    def progress_timeout(pbobj, CFG, self):
+        new_val = pbobj.pbar.get_fraction() + self.intervalo
+
+        if new_val < 0.2:
+            print "en la cola"
+            self.cola = calculo.Cola_Procesos(CFG['nproceso'], CFG['tejecucion'], CFG['tcpu'], CFG['ejecucion'], CFG['cpu'])
+            self.intervalo=0.05
+        if new_val > 0.3 and new_val < 0.35:
+          if CFG['fifo'] == True:
+            CFG['calculofifo'] = calculo.FCFS(self.cola)
+        if new_val > 0.4 and new_val < 0.45:
+          if CFG['menortiempo'] == True:
+            CFG['mtiempo'] = calculo.SJF(self.cola)
+        if new_val > 0.6 and new_val < 0.65:
+          if CFG['roundrobin'] == True:
+            CFG['calculorr'] = calculo.RoundRobin(self.cola, CFG['trr'])
+        if new_val > 0.8 and new_val < 0.85:
+          if CFG['soprtunidad'] == True:
+            CFG['psjf'] = calculo.PSJF(self.cola)
+        if new_val > 1.0:
+            self.lbltitle1.set_text('Resultados de la corrida')
+            self.pbar.hide()
+            self.txt1.set_text("Algoritmos")
+            self.txt2.set_text("Número de procesos")
+            self.txt3.set_text("Uso del CPU")
+            self.txt4.set_text("Tiempo promedio del CPU")
+            self.txt5.set_text("Tiempo promedio de espera")
+            self.txt1.set_text("Algoritmo")
+
+            if CFG['fifo']==True:
+                self.txtfifo.set_text("FCFS")
+                self.txtfifo1.set_text(str(CFG['nproceso']))
+                self.txtfifo2.set_text(str(CFG['calculofifo'][0]))
+                self.txtfifo3.set_text(str(CFG['calculofifo'][1]))
+                self.txtfifo4.set_text(str(CFG['calculofifo'][2]))
+                self.datos = CFG['calculofifo'][3]
+            if CFG['menortiempo']==True:
+                self.txtmt.set_text("SJF")
+                self.txtmt1.set_text(str(CFG['nproceso']))
+                self.txtmt2.set_text(str(CFG['mtiempo'][0]))
+                self.txtmt3.set_text(str(CFG['mtiempo'][1]))
+                self.txtmt4.set_text(str(CFG['mtiempo'][2]))
+                self.datos1 = CFG['mtiempo'][3]
+            if CFG['roundrobin']==True:
+                self.txtrr.set_text("RR")
+                self.txtrr1.set_text(str(CFG['nproceso']))
+                self.txtrr2.set_text(str(CFG['calculorr'][0]))
+                self.txtrr3.set_text(str(CFG['calculorr'][1]))
+                self.txtrr4.set_text(str(CFG['calculorr'][2]))
+            if CFG['soprtunidad']==True:
+                self.txtso.set_text("PSJF")
+                self.txtso1.set_text(str(CFG['nproceso']))
+                self.txtso2.set_text(str(CFG['psjf'][0]))
+                self.txtso3.set_text(str(CFG['psjf'][1]))
+                self.txtso4.set_text(str(CFG['psjf'][2]))
+            CFG['w'].anterior.show()
+            CFG['w'].siguiente.show()
+        pbobj.pbar.set_fraction(new_val)
+
+        return True
